6237fff1a0f926b82a1fe7c72ed2d4632906064e
Jonathan Zaharia <jonathanzaharia@Jonathans-MacBook-Air.local>
2026-03-05 15:02:35 -0500

    Completed exercise in 05-grade.sh

--- 05-grade.sh
+++ 05-grade.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-
 echo "What did you get in the first ITEC 3860 test?"
 echo "Please give a numeric answer"
 read grade
@@ -19,3 +18,15 @@ fi
 # that prints "it's cold" if the temperature is < 40
 # it's chilly if < 60, it's okay if < 70 and, it's hot for 
 # everything else
+echo "What is the current temperature (in Fahrenheit)?"
+read temp
+
+if [ $temp -lt 40 ]; then
+   echo "It's cold"
+elif [ $temp -lt 60 ]; then
+   echo "It's chilly"
+elif [ $temp -lt 70 ]; then
+   echo "It's okay"
+else
+   echo "It's hot"
+fi
